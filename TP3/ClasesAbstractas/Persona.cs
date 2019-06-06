1d0dc763415e2641cfea1e335c5c509872728e66
YesidGuinard <ydguinards@gmail.com>
2019-06-06 20:21:57 -0300

    Inicio TP3

---
+++ TP3/ClasesAbstractas/Persona.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    public abstract class Persona
+    {
+       
+        public enum ENacionalidad { Argentino, Extranjero }
+        #region Atributos
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+        #endregion
+        #region Propiedades
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = this.ValidarNombreApellido(value);
+            }
+        }
+
+        public int DNI
+        {
+            get
+            {
+                return this.dni;
+            }
+            set
+            {
+                this.dni = this.ValidarDni(this.nacionalidad,value);
+            }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get
+            {
+                return this.nacionalidad;
+            }
+            set
+            {
+                this.nacionalidad = value;
+            }
+        }
+        
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = this.ValidarNombreApellido(value);
+            }
+        }
+        
+        public string StringToDNI
+        {
+            set
+            {
+                this.dni = this.ValidarDni(this.nacionalidad, value);
+            }
+
+        }
+        #endregion
+
+        #region Constructores
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        #endregion
+
+             #region Metodos
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            return 0;
+        }
+
+        public int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            return 0;
+        }
+
+        public string ValidarNombreApellido(string dato)
+        {
+            return "";
+        }
+
+        #endregion
+
+
+
+
+
+
+
+    }
+}
---
+++ TP3/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20c6dfba-0bf9-4933-956b-106e6049d782")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
