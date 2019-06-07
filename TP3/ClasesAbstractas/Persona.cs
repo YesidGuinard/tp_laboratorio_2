bad8061ab9936dfbff8a30840ac8a0a0ce427b89
YesidGuinard <ydguinards@gmail.com>
2019-06-06 22:04:14 -0300

    Tp3

--- TP3/ClasesAbstractas/Persona.cs
+++ TP3/ClasesAbstractas/Persona.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Excepciones;
 namespace ClasesAbstractas
 {
     public abstract class Persona
@@ -83,20 +83,54 @@ namespace ClasesAbstractas
 
         public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
         {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacionalidad = nacionalidad;
+        }
 
+        public Persona(string nombre, string apellido,int dni, ENacionalidad nacionalidad): this (nombre,apellido,nacionalidad)
+        {
+            this.DNI = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido,
+            nacionalidad)
+        {
+            this.StringToDNI = dni;
         }
 
         #endregion
 
-             #region Metodos
+        #region Metodos
         public override string ToString()
         {
-            return base.ToString();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre: {0} {1}\n", this.Nombre, this.Apellido);
+            sb.AppendFormat("Dni: {0}\n", this.DNI);
+            sb.AppendFormat("Nacionalidad: {0}\n", this.Nacionalidad);
+            return sb.ToString();
         }
 
         public int ValidarDni(ENacionalidad nacionalidad, int dato)
         {
-            return 0;
+            switch (nacionalidad)
+            {
+                case ENacionalidad.Argentino:
+                    if (dato>=89999999 || dato<=1)
+                    {
+                        throw new NacionalidadInvalidaException("La nacionalidad no coincide con el numero de DNI ");
+                    }
+                    break;
+                case ENacionalidad.Extranjero:
+                    if (dato >= 99999999 || dato <= 90000000)
+                    {
+                        throw new NacionalidadInvalidaException("La nacionalidad no coincide con el numero de DNI ");
+                    }
+                    break;
+                default:
+                    throw  new NacionalidadInvalidaException("Dni fuera de rango");
+            }
+            return dato;
         }
 
         public int ValidarDni(ENacionalidad nacionalidad, string dato)
