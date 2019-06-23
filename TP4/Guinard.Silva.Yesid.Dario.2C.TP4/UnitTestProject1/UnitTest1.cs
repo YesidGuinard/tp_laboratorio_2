ef66ea4a6489137950e349dfced97f744532f19b
YesidGuinard <ydguinards@gmail.com>
2019-06-23 00:34:03 -0300

    Test ok

--- TP4/Guinard.Silva.Yesid.Dario.2C.TP4/UnitTestProject1/UnitTest1.cs
+++ TP4/Guinard.Silva.Yesid.Dario.2C.TP4/UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
 
 namespace UnitTestProject1
 {
@@ -7,8 +8,30 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ListaDePaquetesInstanciada()
         {
+            Correo correo = new Correo();
+            Assert.IsNotNull(correo.Paquetes);
+            
+        }
+
+        [TestMethod]
+        public void PaquetesIguales()
+        {
+            Correo correo = new Correo();
+            Paquete p1 = new Paquete("Misiones 1206", "123");
+            Paquete p2 = new Paquete("Viamonte 765", "123");
+
+            try
+            {
+                correo = correo + p1;
+                correo = correo + p2;
+                Assert.Fail();
+            }catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(TrackingIdRepetidoException));
+            }
+           
         }
     }
 }
